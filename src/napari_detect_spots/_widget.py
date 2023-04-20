545e19c2b869e4a60c6801a42d9f476cfd18599a
Kevin Lai <klai@chanzuckerberg.com>
2023-04-20 11:32:16 -0700

    pushed code for test plugin

--- src/napari_detect_spots/_widget.py
+++ src/napari_detect_spots/_widget.py
@@ -10,33 +10,99 @@ from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from skimage.feature import blob_log
+
+import numpy as np
+from scipy import ndimage as ndi
+
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
+def gaussian_high_pass(image: np.ndarray, sigma: float = 2):
+    """Apply a gaussian high pass filter to an image.
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+    Parameters
+    ----------
+    image : np.ndarray
+        The image to be filtered.
+    sigma : float
+        The sigma (width) of the gaussian filter to be applied.
+        The default value is 2.
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+    Returns
+    -------
+    high_passed_im : np.ndarray
+        The image with the high pass filter applied
+    """
+    low_pass = ndi.gaussian_filter(image, sigma)
+    high_passed_im = image - low_pass
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+    return high_passed_im
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def detect_spots(
+        image: "napari.types.ImageData",
+        high_pass_sigma: float = 2,
+        spot_threshold: float = 0.01,
+        blob_sigma: float = 2
+) -> "napari.types.LayerDataTuple":
+    """Apply a gaussian high pass filter to an image.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        The image in which to detect the spots.
+    high_pass_sigma : float
+        The sigma (width) of the gaussian filter to be applied.
+        The default value is 2.
+    spot_threshold : float
+        The threshold to be passed to the blob detector.
+        The default value is 0.01.
+    blob_sigma: float
+        The expected sigma (width) of the spots. This parameter
+        is passed to the "max_sigma" parameter of the blob
+        detector.
+
+    Returns
+    -------
+    points_coords : np.ndarray
+        An NxD array with the coordinate for each detected spot.
+        N is the number of spots and D is the number of dimensions.
+    sizes : np.ndarray
+        An array of size N, where N is the number of detected spots
+        with the diameter of each spot.
+
+    """
+    # filter the image
+    filtered_spots = gaussian_high_pass(image, high_pass_sigma)
+
+    # detect the spots on the filtered image
+    blobs_log = blob_log(
+        filtered_spots,
+        max_sigma=blob_sigma,
+        num_sigma=1,
+        threshold=spot_threshold
+    )
+
+    # convert the output of the blob detector to the
+    # desired points_coords and sizes arrays
+    # (see the docstring for details)
+    points_coords = blobs_log[:, 0:2]
+    sizes = 2 * np.sqrt(2) * blobs_log[:, 2]
+
+    layer_data = (
+        points_coords,
+        {
+            "face_color": "blue",
+            "symbol": "diamond",
+            "size": sizes
+        },
+        "Points"
+    )
+
+    return layer_data
 
 
 # Uses the `autogenerate: true` flag in the plugin manifest
